b37c4bf579bee128a9352931018b5122f36b1776
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-02-15 15:14:57 -0800

    hope build - removed slug path for notes

--- app/(Pages)/arts/page.tsx
+++ app/(Pages)/arts/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default async function Art() {
     const arts = await getArt();
-    console.log(arts.data);
+    // console.log(arts.data);
     const regex = /(<([^>]+)>)/ig;
   return (
     <>
@@ -14,52 +14,12 @@ export default async function Art() {
                 </div>
             ))
         } 
-        <div className="w-full h-full">
-            <Image src="https://res.cloudinary.com/dryhha34v/image/upload/v1676485418/large_IMG_3383_b2485e68e3.jpg" width={500} height={500} alt="asdas"/>
         
-        </div>
         <div className="w-full h-full">
             <Image src="https://res.cloudinary.com/dryhha34v/image/upload/v1676485634/medium_IMG_3447_0702205fec.jpg" width={500} height={500} alt="asdas"/>
-        
         </div>
         <div className="w-full h-full">
             <Image src="https://res.cloudinary.com/dryhha34v/image/upload/v1676485418/large_IMG_3383_b2485e68e3.jpg" width={500} height={500} alt="asdas"/>
-        
-        </div>
-        {
-            arts.data.map((art) => (
-                <div key={art.id} className="w-full h-full">
-                    <Image src={art.attributes.photograph.data.attributes.formats.medium.url} alt={art.attributes.summary.replace(regex,'')} width={500} height={500} />
-                </div>
-            ))
-        } 
-        <div className="w-full h-full">
-            <Image src="https://res.cloudinary.com/dryhha34v/image/upload/v1676485418/large_IMG_3383_b2485e68e3.jpg" width={500} height={500} alt="asdas"/>
-        
-        </div>
-        <div className="w-full h-full">
-            <Image src="https://res.cloudinary.com/dryhha34v/image/upload/v1676485634/medium_IMG_3447_0702205fec.jpg" width={500} height={500} alt="asdas"/>
-        
-        </div>
-        <div className="w-full h-full">
-            <Image src="https://res.cloudinary.com/dryhha34v/image/upload/v1676485418/large_IMG_3383_b2485e68e3.jpg" width={500} height={500} alt="asdas"/>
-        
-        </div>
-        <div className="w-full h-full">
-            <Image src="https://res.cloudinary.com/dryhha34v/image/upload/v1676485634/medium_IMG_3447_0702205fec.jpg" width={500} height={500} alt="asdas"/>
-        
-        </div>
-        <div className="w-full h-full">
-            <Image src="https://res.cloudinary.com/dryhha34v/image/upload/v1676485418/large_IMG_3383_b2485e68e3.jpg" width={500} height={500} alt="asdas"/>
-        
-        </div>
-        <div className="w-full h-full">
-            <Image src="https://res.cloudinary.com/dryhha34v/image/upload/v1676485634/medium_IMG_3447_0702205fec.jpg" width={500} height={500} alt="asdas"/>
-        
-        </div>
-        <div className="w-full h-full">
-            <Image src="https://res.cloudinary.com/dryhha34v/image/upload/v1676485418/large_IMG_3383_b2485e68e3.jpg" width={500} height={500} alt="asdas"/>
-        
         </div>
     </div>
     </>
