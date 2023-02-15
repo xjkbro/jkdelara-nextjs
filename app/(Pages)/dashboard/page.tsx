c416186e07bc4d5f787bc5dc454a370b773d62c9
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-02-15 09:50:39 -0800

    notes

--- app/(Pages)/dashboard/page.tsx
+++ app/(Pages)/dashboard/page.tsx
@@ -2,40 +2,34 @@
 // import "../../../pages/api/spotify"
 
 export default async function Dashboard() {
-    const {unsplash_data, spotify_data} = await getPageProps();
-    // console.log(spotify_data)
+    // const spotifyData = getSpotifyData();
+    // const unsplashData = getUnsplashData();
+    // const [spotify, unsplash] = await Promise.all([spotifyData, unsplashData]);
 
   return (
     <>
         <div className="h-12 bg-second w-full flex items-center">
             <div className="w-2/3 mx-auto ">
-            <SpotifyPlaying spotify_data={spotify_data} />
+            {/* <SpotifyPlaying data={spotify} /> */}
             </div>
         </div>
     </>
   )
 }
-function SpotifyPlaying({spotify_data}) {
-    // console.log(spotify_data);
-    if(spotify_data.isPlaying == false) {
-        return <a href="https://spotify.com"><i className="fa-brands fa-spotify mr-2"></i><span className="font-bold">Not Playing</span></a>
-    }
-    else
-        return <a href={spotify_data.songUrl}><i className="fa-brands fa-spotify mr-2"></i><span className="font-bold mr-2">Now Playing</span>{spotify_data.artist} - {spotify_data.title}</a>
-}
-async function getPageProps() {
-    // Fetch data from external API
-    const unsplashURL = "development" === process.env.NEXT_PUBLIC_ENV ? "http://localhost:3000/api/unsplash" : "https://jkdelara-nextjs-production.up.railway.app/api/unsplash"
-    const spotifyURL = "development" === process.env.NEXT_PUBLIC_ENV ? "http://localhost:3000/api/spotify" : "https://jkdelara-nextjs-production.up.railway.app/api/spotify"
-    const unsplash = await fetch(unsplashURL, { next: { revalidate: 120 } })
-    const spotify = await fetch(spotifyURL, { next: { revalidate: 120 } })
-    // const unsplash = await fetch(`http://localhost:3000/api/unsplash`)
-    // const spotify = await fetch(`http://localhost:3000/api/spotify`)
-    
-    const unsplash_data = await unsplash.json()
-    const spotify_data = await spotify.json()
-    
-  
-    // Pass data to the page via props
-    return { unsplash_data, spotify_data }
-  }
+// function SpotifyPlaying({data}) {
+//     if(data.isPlaying == false) {
+//         return <a href="https://spotify.com"><i className="fa-brands fa-spotify mr-2"></i><span className="font-bold">Not Playing</span></a>
+//     }
+//     else
+//         return <a href={data.songUrl}><i className="fa-brands fa-spotify mr-2"></i><span className="font-bold mr-2">Now Playing</span>{data.artist} - {data.title}</a>
+// }
+// async function getSpotifyData() {
+//     const spotifyURL = "development" === process.env.NEXT_PUBLIC_ENV ? "http://localhost:3000/api/spotify" : "https://jkdelara-nextjs-production.up.railway.app/api/spotify"
+//     const res = await fetch(spotifyURL, { next: { revalidate: 5 } })
+//     return res.json()
+// }
+// async function getUnsplashData() {
+//     const unsplashURL = "development" === process.env.NEXT_PUBLIC_ENV ? "http://localhost:3000/api/unsplash" : "https://jkdelara-nextjs-production.up.railway.app/api/unsplash"
+//     const res = await fetch(unsplashURL, { next: { revalidate: 3600 } })
+//     return res.json()
+// }
--- app/(Pages)/notes/page.tsx
+++ app/(Pages)/notes/page.tsx
@@ -1,5 +1,18 @@
-export default function Notes() {
+import Link from "next/link"
+export default async function Notes() {
+    const notes = await getNotes();
+    // console.log(notes.data);
   return (
-    <></>
+    <>
+    {notes.data.map((note) => (
+        <Link key={note.id} href={"/notes/"+ note.attributes.slug}>
+            <h3>{note.attributes.title}</h3>
+        </Link>
+    ))}</>
   )
+}
+
+async function getNotes() {
+    const res = await fetch('https://cms.jsondelara.com/api/posts')
+    return res.json();
 }
