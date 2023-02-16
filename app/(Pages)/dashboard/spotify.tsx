e1323de76c0c8659f4091c83e2aa9cf97426a91b
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-02-15 21:24:01 -0800

    note pages

--- app/(Pages)/dashboard/spotify.tsx
+++ app/(Pages)/dashboard/spotify.tsx
@@ -12,23 +12,18 @@ type playingOnSpotify = {
     title?: string
 };
 
-async function fetcher<JSON = any>(
-  input: RequestInfo,
-  init?: RequestInit
-): Promise<JSON> {
+async function fetcher<JSON = any>(input: RequestInfo, init?: RequestInit): Promise<JSON> {
   const res = await fetch(input, init);
   return res.json();
 }
 
 export default function SpotifyPlaying() {
-  const { data } = useSWR<playingOnSpotify[]>('/api/spotify', fetcher);
+  const { data } = useSWR<playingOnSpotify>('/api/spotify', fetcher);
     if(data) {
-        // @ts-ignore
         if(data?.isPlaying == false) {
             return <a href="https://spotify.com"><i className="mr-2 fa-brands fa-spotify"></i><span className="font-bold">Not Playing</span></a>
         }
         else
-        // @ts-ignore
             return <a href={data.songUrl}><i className="mr-2 fa-brands fa-spotify"></i><span className="mr-2 font-bold">Now Playing</span>{data.artist} - {data.title}</a>
     } else 
     return <></>   
---
+++ app/(Pages)/notes/[slug]/page.tsx
@@ -0,0 +1,25 @@
+import useSWR from "swr";
+
+export default async function Note(
+    { params, searchParams }:
+        {
+            params: { slug: string };
+            searchParams?: { [key: string]: string | string[] | undefined };
+        }
+) {
+    const slug: string = params.slug;
+    const note = await getNote(slug);
+    const postData = note.data[0]
+    return (
+        <>
+            <div dangerouslySetInnerHTML={{ __html: postData.attributes.body }} />
+        </>
+    )
+}
+
+async function getNote(slug: string) {
+    const URL = "https://cms.jsondelara.com/api/posts?filters[slug][$eq]=" + slug;
+    console.log(URL)
+    const res = await fetch(URL)
+    return res.json();
+}
