62a0e5a8b92737db8777fb9bdb8f40e66aa9b9f0
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-02-15 16:57:15 -0800

    framer motion

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,25 +3,39 @@ import '../styles/globals.css'
 import '../styles/custom.css'
 import Navigation from '../components/navigation'
 import { useState } from 'react'
+import { motion } from 'framer-motion';
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
     const [isDark, setIsDark] = useState(true)
 
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body className={isDark ? "w-2/3 mx-auto bg-first text-fifth" : "w-2/3 mx-auto bg-fifth text-first"}>
-        <Navigation isDark={isDark} setIsDark={setIsDark} />
-        {children}
-        </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            {/*
+                <head /> will contain the components returned by the nearest parent
+                head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+            */}
+            <head />
+            <motion.body  initial="hidden" animate="visible" variants={{
+                hidden: {
+                    scale: .9,
+                    opacity: 0
+                },
+                visible: {
+                    scale: 1,
+                    opacity: 1,
+                    transition: {
+                    duration: 3
+                    }
+                },
+                }} 
+                className={isDark ? "w-2/3 mx-auto bg-first text-fifth" : "w-2/3 mx-auto bg-fifth text-first"}>
+                <Navigation isDark={isDark} setIsDark={setIsDark} />
+                {children}
+            </motion.body>
+        </html>
+    )
 }
