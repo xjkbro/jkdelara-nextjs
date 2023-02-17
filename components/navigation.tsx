0228d71595cd76db09df4b157d3b1e03dca356fc
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-02-16 17:18:26 -0800

    prose

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -7,7 +7,7 @@ import {faMoon as MoonOff}  from "@fortawesome/free-regular-svg-icons";
 export default function Navigation({isDark, setIsDark}) {
     return (
     <nav className="flex flex-row items-center justify-between my-8">
-        <ul className="flex flex-row overflow-scroll gap-x-8 md:gap-16">
+        <ul className="flex flex-row overflow-scroll md:overflow-auto gap-x-8 md:gap-16">
             <li className="text-lg font-bold hover:text-sixth"><a href="/">Home</a></li>
             <li className="text-lg font-bold hover:text-sixth"><a href="/dashboard">Dashboard</a></li>
             <li className="text-lg font-bold hover:text-sixth"><a href="/work">Work</a></li>
