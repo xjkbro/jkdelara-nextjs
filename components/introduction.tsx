e430895c3be0248d8658d0f9714cba51683f8614
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-02-21 10:11:38 -0800

    typewriter test

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -11,21 +11,22 @@ export default function Introduction() {
             animate={{opacity: 1, translateY: 0}} 
             exit={{opacity: 0, translateY: 50}} 
             transition={{duration: 0.5}}
-            className="z-[5] flex flex-col items-center mx-auto md:justify-between md:h-screen lg:flex-row">
-            <div className="w-full mx-auto my-12 md:w-2/3" id="title">
+            className="z-[5] flex flex-col items-center mx-auto md:justify-between md:h-[80vh] lg:flex-row">
+            <div className="order-2 w-full mx-auto my-8 md:w-fit md:order-1" id="title">
                 <h1 className="text-[2rem] md:text-[3rem] font-black">Jason-Kyle De Lara</h1>
                 <h3 className="text-[1.3rem] md:text-[2rem] font-bold">Full Stack Web Developer at ICP DAS USA.</h3>
-                <motion.div 
+                <motion.a 
                     whileHover={{ scale: 1.05 }}
                     whileTap={{ scale: 0.95 }}
                     drag
                     transition={{ type: "spring", stiffness: 400, damping: 10 }}
-                    className="font-bold text-[1.5rem] bg-eighth hover:bg-seventh rounded-full p-2 w-24 text-center mt-2">
-                    <a href="/resume2022b.pdf" download><FontAwesomeIcon icon={faAngleDoubleDown} className="mr-2" />CV</a>
-                </motion.div>
+                    className="block font-bold text-[1.5rem] bg-eighth hover:bg-seventh rounded-full p-2 w-24 text-center mt-2"
+                    href="/resume2022b.pdf" download>
+                        <FontAwesomeIcon icon={faAngleDoubleDown} className="mr-2" />CV
+                </motion.a>
             </div>
-            <div id="home-avatar" className="w-full mx-auto my-12 md:w-1/3 h-1/3 " >
-                <Image className="mx-auto bg-transparent rounded-full" src="/me.JPG" priority width={300} height={300} alt="Photo of Jason Kyle De Lara" />
+            <div id="home-avatar" className="mx-auto my-8 w-[33vh] h-[33vh]  md:order-2 order-1" >
+                <AnimatedBorder><Image className="mx-auto bg-transparent rounded-full" src="/me.JPG" priority width={300} height={300} alt="Photo of Jason Kyle De Lara" /></AnimatedBorder>
             </div>
         </motion.div>
         {/* <div className="overflow-hidden z-[2] ">
@@ -49,3 +50,13 @@ export default function Introduction() {
   )
 }
 
+
+function AnimatedBorder({children}){
+    return (
+        <div className="animate-border rounded-full h-full bg-white from-teal-500 via-purple-500 to-pink-500 bg-[length:400%_400%] p-1 transition bg-gradient-to-r">
+            <div className="w-full h-full transition-all rounded-full dark:bg-second bg-fifth">
+                {children}
+            </div>
+        </div>
+    )
+}
