6bbc34edd5270e5a907583386b7d4ee7fd844a70
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-02-21 15:04:37 -0800

    base64

--- app/(Pages)/arts/imageModal.tsx
+++ app/(Pages)/arts/imageModal.tsx
@@ -3,6 +3,32 @@ import EXIF from 'exif-js';
 import { useState, useEffect} from 'react';
 import Image from 'next/image';
 import { motion, AnimatePresence } from 'framer-motion';
+const toBase64 = (str: string) => {
+    return typeof window === "undefined"
+      ? Buffer.from(str).toString("base64")
+      : window.btoa(str);
+  };
+  
+  export const createSvgShimmer = (width: number, height: number): string => {
+    const shimmer = `
+          <svg width="${width}" height="${height}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+              <defs>
+                  <linearGradient id="g">
+                      <stop stop-color="#333" offset="20%" />
+                      <stop stop-color="#222" offset="50%" />
+                      <stop stop-color="#333" offset="70%" />
+                  </linearGradient>
+              </defs>
+              <rect width="${width}" height="${height}" fill="#333" />
+              <rect id="r" width="${width}" height="${height}" fill="url(#g)" />
+              <animate xlink:href="#r" attributeName="x" from="-${width}" to="${width}" dur="1s" repeatCount="indefinite" />
+          </svg>
+      `;
+  
+    return `data:image/svg+xml;base64,${toBase64(shimmer)}`;
+  };
+
+  
 
 export default function ImageModal({art, i}) {
     const [modalImgUrl, setModalImgUrl] = useState(null);
@@ -19,6 +45,7 @@ export default function ImageModal({art, i}) {
         //     })
         // }
     }, [modalImgUrl]);
+    
     return (
         <AnimatePresence>
             {
@@ -38,7 +65,7 @@ export default function ImageModal({art, i}) {
                                 className=" flex flex-col justify-center items-center w-[90%] md:w-fit"
                                 onClick={()=> setModalImgUrl(null)} 
                                 >
-                                <Image src={modalImgUrl} alt={"artbyjkd "+art?.attributes?.summary?.replace(regex,'')} className="!h-[95%]"  width={1000} height={1000} quality={100} placeholder='blur' blurDataURL='/spinner.svg' />
+                                <Image src={modalImgUrl} alt={"artbyjkd "+art?.attributes?.summary?.replace(regex,'')} className="!h-[95%]"  width={1000} height={1000} quality={100} placeholder='blur' blurDataURL={createSvgShimmer(748, 497)} />
                                 <div className="h-8">
                                     {/* EXIF DATA DISPLAY */}
                                 </div>
