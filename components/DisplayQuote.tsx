69c1a4ad4a10523acd527014dd66f7e94d70d7e5
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-03-06 13:45:57 -0800

    quote animation

--- components/DisplayQuote.tsx
+++ components/DisplayQuote.tsx
@@ -1,22 +1,69 @@
+'use client'
+import { motion, Variants } from "framer-motion"
 import { Catamaran } from "@next/font/google"
 const QuoteFont = Catamaran({ weight: ['500', '700', '900'], subsets: ['latin'] })
 
+const lQuoteVariants: Variants = {
+    offscreen: {
+      x: -50,
+      opacity:0.5
+    },
+    onscreen: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5
+      }
+    }
+  };
+const rQuoteVariants: Variants = {
+    offscreen: {
+      x: 50,
+      opacity:0.5
+    },
+    onscreen: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.5
+      }
+    }
+};
+const quoteVariants: Variants = {
+    offscreen: {
+      y: 100,
+      opacity:0.5
+    },
+    onscreen: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.4
+      }
+    }
+};
 
 export default function DisplayQuote({quote, quotee}) {
   return (
-    <div className="flex items-center justify-center py-16 h-fit">
+    <motion.div
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: false, amount: 0.8 }}
+                className="flex items-center justify-center py-16 h-fit">
         <div className={"grid grid-cols-3 md:grid-cols-[200px_minmax(900px,_1fr)_100px] mx-auto text-center align-middle " + QuoteFont.className}>
-            <div className="text-[5rem] md:text-[10rem] h-16 md:h-32 text-eighth font-black">&ldquo;</div>
+            <motion.div variants={lQuoteVariants} className="text-[5rem] md:text-[10rem] h-16 md:h-32 text-eighth font-black">&ldquo;</motion.div>
             <div className="col-span-2"></div>
             <div className="hidden md:block md:col-span-1"></div>
-            <div className="col-span-3 text-center md:col-span-1">
+            <motion.div 
+                variants={quoteVariants}
+                className="col-span-3 text-center md:col-span-1">
                 <div className="text-lg md:text-[2.4rem] break-normal font-bold md:leading-9">{quote}</div>
                 <div className="mt-2 text-sm italic font-normal md:mt-8 md:text-1rem text-first dark:text-fourth">- {quotee}</div>
-            </div>
+            </motion.div>
             <div className="hidden md:block md:col-span-1"></div>
             <div className="col-span-2"></div>
-            <div className="text-[5rem] md:text-[10rem] h-32 md:h-64 text-eighth font-black">&rdquo;</div>
+            <motion.div variants={rQuoteVariants}  className="text-[5rem] md:text-[10rem] h-32 md:h-64 text-eighth font-black">&rdquo;</motion.div>
         </div>
-    </div>
+    </motion.div>
   )
 }
