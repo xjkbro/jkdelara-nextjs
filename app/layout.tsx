91fc567f6720a97d2e21bf596fdf9b5d43a507b6
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-05-08 14:15:36 -0700

    ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import ClientPageWrapper from "@/components/ClientPageWrapper";
 import { meta } from "@/constants/meta";
 import FramerBG from "@/components/FramerBG";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 export const metadata = {
     ...meta,
     title: {
@@ -20,6 +21,13 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" className=" antialiased scroll-smooth">
+            <Script
+                id="Adsense-id"
+                data-ad-client={process.env.GOOGLE_ADS_KEY}
+                async
+                // strategy="beforeInteractive"
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+            />
             <ClientPageWrapper>
                 <main>
                     {children}
