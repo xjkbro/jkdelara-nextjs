d50dc0886e6d3cd96d95018f991210f7923c8d7d
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-05-17 10:52:18 -0700

    animate timeline

--- app/(Pages)/story/page.tsx
+++ app/(Pages)/story/page.tsx
@@ -1,7 +1,5 @@
 // import Timeline from "./timeline";
-import clsx from "clsx";
-import { Roboto_Slab } from "next/font/google";
-const robotoSlab = Roboto_Slab({ subsets: ["latin"] });
+import StoryPanel from "./story";
 
 export const metadata = { title: "Story" };
 
@@ -16,41 +14,7 @@ export default async function Story() {
             : 0
     );
 
-    return (
-        <div className="w-11/12 mx-auto h-[70vh] snap-mandatory snap-y overflow-y-scroll scrollbar-hide rounded-xl">
-            {stories.data.map((story, i) => (
-                <section
-                    key={i}
-                    className="my-2 h-[70vh] bg-eighth/90 text-black snap-center p-4 py-12 md:p-24 rounded-xl relative"
-                >
-                    <div className="flex justify-start items-center h-full">
-                        <div className="hidden md:block w-96">
-                            {DisplayDateRange(story)}
-                        </div>
-                        <div className=" md:-translate-x-10 w-full">
-                            <h2
-                                className={clsx(
-                                    "text-5xl break-words mb-3 md:text-6xl font-black text-first",
-                                    robotoSlab.className
-                                )}
-                            >
-                                {story.attributes.name}
-                            </h2>
-                            <div className="md:hidden block w-96">
-                                {DisplayDateRange(story)}
-                            </div>
-                            <h4 className="text-2xl md:text-3xl mb-3 font-semibold text-third">
-                                {story.attributes.title}
-                            </h4>
-                            <p className="text-sm font-normal font-mono">
-                                {story.attributes.summary}
-                            </p>
-                        </div>
-                    </div>
-                </section>
-            ))}
-        </div>
-    );
+    return <StoryPanel stories={stories} />;
     // return (
     //     <div className="w-[90vw] md:w-2/3 mx-auto">
     //         <Timeline stories={stories} />
@@ -65,33 +29,3 @@ async function getStories() {
     );
     return res.json();
 }
-
-const DisplayDateRange = (story) => {
-    const startDate = new Date(story.attributes.started);
-    const start = startDate.toLocaleDateString("en-US", {
-        year: "numeric",
-        // month: "short",
-        // day: "numeric",
-    });
-
-    let end: Date | string = "Present";
-    if (story.attributes.ended != null) {
-        end = new Date(story.attributes.ended);
-        end = end.toLocaleDateString("en-US", {
-            year: "numeric",
-            // month: "long",
-            // day: "numeric",
-        });
-    }
-    return (
-        <div className="flex md:flex-col items-center md:justify-center md:p-12 md:w-72 md:h-72 rounded-full md:bg-first md:opacity-30">
-            <div className="text-slate-200 font-bold text-xl md:text-3xl">
-                {start}
-            </div>
-            <div className="text-white font-black">-</div>
-            <div className="text-slate-200 font-bold text-xl md:text-3xl">
-                {end}
-            </div>
-        </div>
-    );
-};
--- app/(Pages)/story/story.tsx
+++ app/(Pages)/story/story.tsx
@@ -1,43 +1,86 @@
-"use client"
+"use client";
+import clsx from "clsx";
 import { motion } from "framer-motion";
+import { Roboto_Slab } from "next/font/google";
+const robotoSlab = Roboto_Slab({ subsets: ["latin"] });
 
-export default function SingleStory({ story , i }) {
-    const regex = /(<([^>]+)>)/ig;
-    const getDateRange = (story) => {
-        const startDate = new Date(story.attributes.started);
-        const start = startDate.toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" });
-
-        let end: Date | string = "Present";
-        if (story.attributes.ended != null) {
-            end = new Date(story.attributes.ended)
-            end = end.toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" })
-        }
-        return "From " + start + " to " + end
-    }
-    
-    const item = {
-        hidden: { opacity: 0 },
-        show: { opacity: 1 }
-    }
+export default function StoryPanel({ stories }) {
     return (
-        <motion.div 
+        <motion.div
             initial={{
                 opacity: 0,
-                translateY: 50
+                translateY: 50,
             }}
-            animate={{opacity:1, translateY:0}}
-            transition={{duration: 0.5, delay: i * 0.2}}
-            className="py-4 mb-2 border-l-4 border-eighth">
-            <div className="">
-                {/* <span className="ml-2 text-lg font-bold">{story.attributes.title}</span> */}
-                <span className="text-lg font-bold md:ml-8">{story.attributes.name}</span>
-                <span className="block text-sm md:inline md:ml-2 dark:text-fourth text-third">{getDateRange(story)}</span>
-            </div>
-            <span className="mb-2 text-sm font-bold md:mx-8">{story.attributes.title}</span>
-            {/* <span className="px-2 py-1 ml-2 text-sm rounded-full bg-eighth text-fifth">{story.attributes.title}</span> */}
-            <p className="md:mx-8">{story.attributes.summary}</p>
+            animate={{ opacity: 1, translateY: 0 }}
+            transition={{
+                type: "spring",
+                stiffness: 100,
+                duration: 1,
+                delay: 0.1,
+            }}
+            className="w-11/12 mx-auto h-[70vh] snap-mandatory snap-y overflow-y-scroll scrollbar-hide rounded-xl"
+        >
+            {stories.data.map((story, i) => (
+                <section
+                    key={i}
+                    className="my-2 h-[70vh] bg-eighth/90 text-black snap-center p-4 py-12 md:p-24 rounded-xl relative"
+                >
+                    <div className="flex justify-start items-center h-full">
+                        <div className="hidden md:block w-96">
+                            {DisplayDateRange(story)}
+                        </div>
+                        <div className=" md:-translate-x-10 w-full">
+                            <h2
+                                className={clsx(
+                                    "text-5xl break-words mb-3 md:text-6xl font-black text-first",
+                                    robotoSlab.className
+                                )}
+                            >
+                                {story.attributes.name}
+                            </h2>
+                            <div className="md:hidden block w-96">
+                                {DisplayDateRange(story)}
+                            </div>
+                            <h4 className="text-2xl md:text-3xl mb-3 font-semibold text-third">
+                                {story.attributes.title}
+                            </h4>
+                            <p className="text-sm font-normal font-mono">
+                                {story.attributes.summary}
+                            </p>
+                        </div>
+                    </div>
+                </section>
+            ))}
         </motion.div>
-    )
+    );
 }
 
+const DisplayDateRange = (story) => {
+    const startDate = new Date(story.attributes.started);
+    const start = startDate.toLocaleDateString("en-US", {
+        year: "numeric",
+        // month: "short",
+        // day: "numeric",
+    });
 
+    let end: Date | string = "Present";
+    if (story.attributes.ended != null) {
+        end = new Date(story.attributes.ended);
+        end = end.toLocaleDateString("en-US", {
+            year: "numeric",
+            // month: "long",
+            // day: "numeric",
+        });
+    }
+    return (
+        <div className="flex md:flex-col items-center md:justify-center md:p-12 md:w-72 md:h-72 rounded-full md:bg-first md:opacity-30">
+            <div className="text-slate-200 font-bold text-xl md:text-3xl">
+                {start}
+            </div>
+            <div className="text-white font-black">-</div>
+            <div className="text-slate-200 font-bold text-xl md:text-3xl">
+                {end}
+            </div>
+        </div>
+    );
+};
