4dafdf4c7e04316ee31365a367c028e10f154841
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-05-18 13:43:16 -0700

    mdx

--- components/SkillBlock.jsx
+++ components/SkillBlock.jsx
@@ -6,7 +6,7 @@ const skills = [
     { type: "CSS/Tailwind", level: 80 },
     { type: "Javascript", level: 85 },
     { type: "Typescript", level: 45 },
-    { type: "React/NextJS", level: 55 },
+    { type: "React/NextJS", level: 65 },
     { type: "PHP", level: 75 },
     { type: "NodeJS", level: 45 },
     { type: "WordPress", level: 60 },
