bcdd138c7b12253ee3bd542f44e9f0c5f96ec9d8
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-06-07 19:54:51 -0700

    yt

--- constants/socials.ts
+++ constants/socials.ts
@@ -1,46 +1,46 @@
 export const socials = {
-    data: [
-        {
-            id: 1,
-            attributes: {
-                name: "Instagram",
-                url: "https://instagram.com/jkbro",
-            },
-        },
-        {
-            id: 1,
-            attributes: {
-                name: "Twitter",
-                url: "https://twitter.com/json_delara",
-            },
-        },
-        {
-            id: 2,
-            attributes: {
-                name: "LinkedIn",
-                url: "https://linkedin.com/in/jkdelara",
-            },
-        },
-        {
-            id: 3,
-            attributes: {
-                name: "YouTube",
-                url: "https://youtube.com/@xjustkiddingbro",
-            },
-        },
-        {
-            id: 4,
-            attributes: {
-                name: "Unsplash",
-                url: "https://unsplash.com/@jkdelara",
-            },
-        },
-        {
-            id: 5,
-            attributes: {
-                name: "GitHub",
-                url: "https://github.com/xjkbro",
-            },
-        },
-    ],
+	data: [
+		{
+			id: 1,
+			attributes: {
+				name: "Instagram",
+				url: "https://instagram.com/jkbro",
+			},
+		},
+		{
+			id: 1,
+			attributes: {
+				name: "Twitter",
+				url: "https://twitter.com/json_delara",
+			},
+		},
+		{
+			id: 2,
+			attributes: {
+				name: "LinkedIn",
+				url: "https://linkedin.com/in/jkdelara",
+			},
+		},
+		{
+			id: 3,
+			attributes: {
+				name: "YouTube",
+				url: "https://youtube.com/@jkbrodotdev",
+			},
+		},
+		{
+			id: 4,
+			attributes: {
+				name: "Unsplash",
+				url: "https://unsplash.com/@jkdelara",
+			},
+		},
+		{
+			id: 5,
+			attributes: {
+				name: "GitHub",
+				url: "https://github.com/xjkbro",
+			},
+		},
+	],
 };
