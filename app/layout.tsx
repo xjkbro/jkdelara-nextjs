97b7b47b746ceb835498622144222fb7f2d92e82
Jason-Kyle De Lara <jason8oh8@gmail.com>
2023-06-14 11:34:53 -0700

    revert

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,33 +7,33 @@ import FramerBG from "@/components/FramerBG";
 import { Analytics } from "@vercel/analytics/react";
 import Script from "next/script";
 export const metadata = {
-    ...meta,
-    title: {
-        default: "JKDELARA",
-        template: "%s | Jason-Kyle De Lara",
-    },
+	...meta,
+	title: {
+		default: "JKDELARA",
+		template: "%s | Jason-Kyle De Lara",
+	},
 };
 
 export default function RootLayout({
-    children,
+	children,
 }: {
-    children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-    return (
-        <html lang="en" className=" antialiased scroll-smooth">
-            {/* <Script
+	return (
+		<html lang="en" className=" antialiased scroll-smooth">
+			{/* <Script
                 id="Adsense-id"
                 async
                 // strategy="beforeInteractive"
                 src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.GOOGLE_ADS_KEY}`}
             /> */}
-            <ClientPageWrapper>
-                <main>
-                    {children}
-                    {/* <FramerBG /> */}
-                </main>
-            </ClientPageWrapper>
-            <Analytics />
-        </html>
-    );
+			<ClientPageWrapper>
+				<main>
+					{children}
+					<FramerBG />
+				</main>
+			</ClientPageWrapper>
+			<Analytics />
+		</html>
+	);
 }
--- components/ClientPageWrapper.tsx
+++ components/ClientPageWrapper.tsx
@@ -6,51 +6,51 @@ import Footer from "@/components/footer";
 import Background from "./Background";
 
 export default function ClientPageWrapper({ children }) {
-    /* ==== Dark Mode Set Up ===== */
-    let boolDark = true;
-    const [isDark, setIsDark] = useState(boolDark);
+	/* ==== Dark Mode Set Up ===== */
+	let boolDark = true;
+	const [isDark, setIsDark] = useState(boolDark);
 
-    useEffect(() => {
-        const storedDarkMode = localStorage.getItem("DARK_MODE");
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        storedDarkMode == "false" ? (boolDark = false) : (boolDark = true);
-        localStorage.setItem("DARK_MODE", String(boolDark));
-        setIsDark(boolDark);
-    }, [boolDark]);
+	useEffect(() => {
+		const storedDarkMode = localStorage.getItem("DARK_MODE");
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		storedDarkMode == "false" ? (boolDark = false) : (boolDark = true);
+		localStorage.setItem("DARK_MODE", String(boolDark));
+		setIsDark(boolDark);
+	}, [boolDark]);
 
-    useEffect(() => {
-        localStorage.setItem("DARK_MODE", String(isDark));
-    }, [isDark]);
-    /* =========================== */
-    const [overlay, setOverlay] = useState(false);
-    return (
-        <AnimatePresence>
-            <motion.body
-                className={
-                    isDark
-                        ? "dark bg-first text-fifth transition-colors"
-                        : "bg-fifth text-first transition-colors"
-                }
-            >
-                <motion.div>
-                    <Navigation
-                        isDark={isDark}
-                        setIsDark={setIsDark}
-                        overlay={overlay}
-                        setOverlay={setOverlay}
-                    />
-                    <div
-                        className={
-                            isDark
-                                ? "h-[100vh] fixed -z-20 top-0 left-0 w-full bg-gradient-to-t from-first to-transparent"
-                                : "h-[100vh] fixed -z-20 top-0 left-0 w-full bg-gradient-to-t from-fifth to-transparent"
-                        }
-                    />
-                    <Background dark={isDark} />
-                    {children}
-                    <Footer />
-                </motion.div>
-            </motion.body>
-        </AnimatePresence>
-    );
+	useEffect(() => {
+		localStorage.setItem("DARK_MODE", String(isDark));
+	}, [isDark]);
+	/* =========================== */
+	const [overlay, setOverlay] = useState(false);
+	return (
+		<AnimatePresence>
+			<motion.body
+				className={
+					isDark
+						? "dark bg-first text-fifth transition-colors"
+						: "bg-fifth text-first transition-colors"
+				}
+			>
+				<motion.div>
+					<Navigation
+						isDark={isDark}
+						setIsDark={setIsDark}
+						overlay={overlay}
+						setOverlay={setOverlay}
+					/>
+					<div
+						className={
+							isDark
+								? "h-[100vh] fixed -z-20 top-0 left-0 w-full bg-gradient-to-t from-first to-transparent"
+								: "h-[100vh] fixed -z-20 top-0 left-0 w-full bg-gradient-to-t from-fifth to-transparent"
+						}
+					/>
+					{/* <Background dark={isDark} /> */}
+					{children}
+					<Footer />
+				</motion.div>
+			</motion.body>
+		</AnimatePresence>
+	);
 }
